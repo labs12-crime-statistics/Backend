8a74c5c7def96c4e574e57e27f8dcc7738a819a4
Michael Beck <brit228@gmail.com>
2019-05-03 14:36:32 -0400

    Added two routes and updated models

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 """Contains models for DB."""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Column, BigInteger, Integer, String, DateTime, \
+    ForeignKey, Float, LargeBinary
+from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
 
@@ -11,41 +13,75 @@ BASE = declarative_base()
 class City(BASE):
     """City model for DB. Has information of cities."""
     __tablename__ = 'city'
-    id            = Column(Integer, primary_key=True)
+    id            = Column(BigInteger, primary_key=True)
     city          = Column(String, unique=False, nullable=False)
     state         = Column(String, unique=False, nullable=True)
     country       = Column(String, unique=False, nullable=False)
-    zipcode       = Column(String, unique=False, nullable=False)
     location      = Column(Geometry(geometry_type='POINT'), nullable=False)
+    blocks        = relationship("Blocks", back_populates="city")
+    zipcodes      = relationship("ZipcodeGeom", back_populates="city")
+    incidents     = relationship("Incident", back_populates="city")
 
 
 class Blocks(BASE):
     """Block model for DB. Has information of city blocks for a related city
         id."""
     __tablename__ = 'block'
-    id            = Column(Integer, primary_key=True)
-    cityid        = Column(Integer, ForeignKey('city.id'), nullable=False)
+    id            = Column(BigInteger, primary_key=True)
+    cityid        = Column(BigInteger, ForeignKey('city.id'), nullable=False)
     shape         = Column(Geometry(geometry_type='MULTIPOLYGON'), nullable=False)
     population    = Column(Integer, nullable=False)
+    prediction    = Column(LargeBinary, nullable=True)
+    city          = relationship("City", back_populates="blocks")
+    incidents     = relationship("Incident", back_populates="block")
 
+class ZipcodeGeom(BASE):
+    """Zipcode geometry model for DB. Has information of zipcodes and related
+        city id."""
+    __tablename__ = 'zipcodegeom'
+    id            = Column(BigInteger, primary_key=True)
+    cityid        = Column(BigInteger, ForeignKey('city.id'), nullable=False)
+    zipcode       = Column(String, nullable=False, unique=True)
+    shape         = Column(Geometry(geometry_type='MULTIPOLYGON'), nullable=False)
+    city          = relationship("City", back_populates="zipcodes")
 
 class Incident(BASE):
     """Incident model for DB. Has information of a specific crime, including
         where it took place, when it took place, and the type of crime that
         occurred."""
     __tablename__ = 'incident'
-    id            = Column(Integer, primary_key=True)
-    crimetypeid   = Column(Integer, ForeignKey('crimetype.id'), nullable=False)
-    cityid        = Column(Integer, ForeignKey('city.id'), nullable=False)
-    blockid       = Column(Integer, ForeignKey('block.id'), nullable=False)
+    id            = Column(BigInteger, primary_key=True)
+    crimetypeid   = Column(BigInteger, ForeignKey('crimetype.id'), nullable=False)
+    locdescid     = Column(BigInteger, ForeignKey('locdesctype.id'), nullable=False)
+    cityid        = Column(BigInteger, ForeignKey('city.id'), nullable=False)
+    blockid       = Column(BigInteger, ForeignKey('block.id'), nullable=False)
     location      = Column(Geometry(geometry_type='POINT'), nullable=False)
     datetime      = Column(DateTime, nullable=False)
-
+    hour          = Column(Integer, nullable=False)
+    dow           = Column(Integer, nullable=False)
+    month         = Column(Integer, nullable=False)
+    year          = Column(Integer, nullable=False)
+    city          = relationship("City", back_populates="incidents")
+    block         = relationship("Blocks", back_populates="incidents")
+    crimetype     = relationship("CrimeType", back_populates="incidents")
+    locationdesc  = relationship("LocationDescriptionType", back_populates="incidents")
 
 class CrimeType(BASE):
     """CrimeType model for DB. Has information of the types of crime, including
         a general description and the numerical severity of the crime."""
     __tablename__ = 'crimetype'
-    id            = Column(Integer, primary_key=True)
+    id            = Column(BigInteger, primary_key=True)
     category      = Column(String, unique=True, nullable=False)
-    severity      = Column(Float, nullable=False)
+    severity      = Column(Integer, nullable=False)
+    incidents     = relationship("Incident", back_populates="crimetype")
+
+
+class LocationDescriptionType(BASE):
+    """Location description model for DB. Has information on the type of
+        location that the crime took place."""
+    __tablename__ = 'locdesctype'
+    id            = Column(BigInteger, primary_key=True)
+    key1          = Column(String, nullable=False)
+    key2          = Column(String, nullable=False)
+    key3          = Column(String, nullable=False)
+    incidents     = relationship("Incident", back_populates="locationdesc")
