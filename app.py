c43ef9f6e72d237b31a03790e35d67ab284de543
Michael Beck <brit228@gmail.com>
2019-05-09 23:54:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -166,9 +166,9 @@ def get_predict_data(cityid):
     all_dates = []
     block_date = {}
     for row in SESSION.execute(text(query), {"cityid": cityid}).fetchall():
-        prediction[int(r[0])] = (np.frombuffer(bytes.fromhex(row[1]), dtype=np.float64).reshape((12,7,24)) / maxseverity)**0.1
-        block_date[int(r[0])] = int(r[3])*12+int(r[2])-1
-        all_dates.append(int(r[3])*12+int(r[2])-1)
+        prediction[int(row[0])] = (np.frombuffer(bytes.fromhex(row[1]), dtype=np.float64).reshape((12,7,24)) / maxseverity)**0.1
+        block_date[int(row[0])] = int(row[3])*12+int(row[2])-1
+        all_dates.append(int(row[3])*12+int(row[2])-1)
     all_dates = sorted(list(set(all_dates)))
     predictions_n = {}
     predictionall = np.zeros((len(all_dates),7,24))
