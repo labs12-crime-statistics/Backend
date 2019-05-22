666a621bbdea98b2d06883d98af14a7d1d7028c2
Michael Beck <brit228@gmail.com>
2019-05-22 15:40:30 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -407,4 +407,4 @@ def get_data(config_dict, blockid, dotw, crimeviolence, crimeppos, locgroups):
         SESSION.add(job)
         SESSION.commit()
         return job.id
-    return cofig_dict["loadtype"]
+    return config_dict["loadtype"]
