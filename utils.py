b495b17ee875436350782a07f873b7572865b6ef
Michael Beck <brit228@gmail.com>
2019-05-24 08:21:40 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -126,7 +126,7 @@ def get_tips(config_dict):
     future_h = (future_h - future_h.mean()) * 24
 
     crime_change_past = 0.2 * (crime_block_curr - crime_block_past) / crime_block_past
-    crime_change_pred = (12 * 0.000001 * crime_future.sum() - crime_block_curr) / crime_block_curr
+    crime_change_pred = (0.000001 * crime_future.sum() - crime_block_curr) / crime_block_curr
     std_val = (crime_block_curr - crime_all_curr) / crime_all_std
 
     result = {
